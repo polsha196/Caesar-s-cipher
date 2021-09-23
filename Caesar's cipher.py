001ae71b115c215640f30936a2ca5068f76286db
polsha196 <90346112+polsha196@users.noreply.github.com>
2021-09-23 17:29:19 +0500

    Исправлены ошибки
    Добавлен проблем между словами

---
+++ Caesar's cipher.py
@@ -0,0 +1,33 @@
+
+alpha = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+alpha=list(alpha)
+s=int(input("Введите число: 1-шифровка, 0-расшифровка: "))
+c=input("Введите строку: ")
+n=int(input("Число - кол-во сдвигов: "))
+c=list(c)
+c_len=len(c)
+l=33
+res=''
+res_1=''
+if s == 1:
+    for k in range(0, c_len):
+        if c[k] == ' ':
+            res+=' '
+        for i in range(0, len(alpha)):
+            if c[k]==alpha[i]:
+                res+=(alpha[(i+n)%l])
+   
+else:
+    for k in range(0, c_len):
+        if c[k] == ' ':
+            res+=' '
+        for i in range(0, len(alpha)):
+            if c[k]==alpha[i]:
+                res+=(alpha[(i-n)%l])
+print(res)
+        
+
+
+
+
+    
