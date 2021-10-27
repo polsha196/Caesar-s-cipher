8fa6af7bab1d2bc2a6cd378bdcb49fde447937e6
polsha196 <90346112+polsha196@users.noreply.github.com>
2021-10-27 13:55:25 +0500

    Проверка и комментарии
    Добавлены проверка входных данных и комментарии

--- Caesar's cipher.py
+++ Caesar's cipher.py
@@ -1,64 +1,37 @@
-<<<<<<< HEAD
-
-alpha = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-alpha=list(alpha)
-s=int(input("Введите число: 1-шифровка, 0-расшифровка: "))
-c=input("Введите строку: ")
-n=int(input("Число - кол-во сдвигов: "))
-c=list(c)
-c_len=len(c)
-l=33
+alpha = list( 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+space=list(' ')
 res=''
-res_1=''
-if s == 1:
-    for k in range(0, c_len):
-        if c[k] == ' ':
-            res+=' '
-        for i in range(0, len(alpha)):
-            if c[k]==alpha[i]:
-                res+=(alpha[(i+n)%l])
-   
-else:
-    for k in range(0, c_len):
-        if c[k] == ' ':
-            res+=' '
-        for i in range(0, len(alpha)):
-            if c[k]==alpha[i]:
-                res+=(alpha[(i-n)%l])
-print(res)
-        
+check=''
+while True: #проверка, что вводится число
+    n=input("Число - кол-во сдвигов: ")
+    try:
+        val=int(n)
+        break
+    except ValueError:
+        print("Введено некорректное число! Попробуйте еще раз ")
+n=int(n)
+while True: #проверка фразы на наличие неправильных символов
+    phrase=input("Введите строку: ")
+    phrase=list(phrase)
+    ph_len=len(phrase)
+    union=list(set(alpha+space)) #объединение элементов из алфавита и пробела
+    check=list(set(phrase)-set(union))
+    if check == list(''):
+       for k in range(0, ph_len): 
+             if phrase[k] == ' ':
+                 res+=' '
+             for i in range(0, len(alpha)):
+                if phrase[k]==alpha[i]:
+                  res+=alpha[(i+n)%33]
+       print(res)
+       break     
+    else:
+        print("Введена некоректная строка! Попробуйте еще раз")
 
 
 
 
-=======
-
-alpha = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-alpha=list(alpha)
-s=int(input("Введите число: 1-шифровка, 0-расшифровка: "))
-c=input("Введите строку: ")
-n=int(input("Число - кол-во сдвигов: "))
-c=list(c)
-c_len=len(c)
-l=33
-res=''
-res_1=''
-if s == 1:
-    for k in range(0, c_len):
-        for i in range(0, len(alpha)):
-            if c[k]==alpha[i]:
-                res+=(alpha[(i+n)%l])
-   
-else:
-    for k in range(0, c_len):
-        for i in range(0, len(alpha)):
-            if c[k]==alpha[i]:
-                res+=(alpha[(i-n)%l])
-print(res)
-        
 
 
 
-
->>>>>>> 98177977ee399a6c1b5b7e0984a69d36b87b1400
     
